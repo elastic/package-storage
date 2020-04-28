d64e74e7cd7778497be4d347217810cafabc0e54
Nicolas Ruflin <spam@ruflin.com>
2020-04-28 10:47:18 +0200

    Add magefile to validate packages (#3)
    Running `mage build` will validate the packages. This is not added to an CI yet but should be added in the future.
    
    This PR also updates the packages to follow the most recent changes.

---
+++ magefile.go
@@ -0,0 +1,49 @@
+// Copyright Elasticsearch B.V. and/or licensed to Elasticsearch B.V. under one
+// or more contributor license agreements. Licensed under the Elastic License;
+// you may not use this file except in compliance with the Elastic License.
+
+// +build mage
+
+package main
+
+import (
+	"os"
+	"path/filepath"
+	"strconv"
+
+	"github.com/magefile/mage/sh"
+)
+
+var (
+	buildDir     = "./build"
+	publicDir    = filepath.Join(buildDir, "public")
+	packagePaths = []string{"./packages"}
+	tarGz        = true
+)
+
+func Build() error {
+
+	err := os.RemoveAll(publicDir)
+	if err != nil {
+		return err
+	}
+
+	err = os.MkdirAll(publicDir, 0755)
+	if err != nil {
+		return err
+	}
+
+	sh.Run("go", "get", "-u", "github.com/elastic/package-registry/dev/generator")
+	for _, p := range packagePaths {
+		err := sh.Run("generator", "-sourceDir="+p, "-publicDir="+publicDir, "-tarGz="+strconv.FormatBool(tarGz))
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func Clean() error {
+	return os.RemoveAll(buildDir)
+}
