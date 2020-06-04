2d521c5def4481185b48ca983ca5c0dea25a124e
mtojek <marcin.tojek@elastic.co>
2020-06-04 12:42:57 +0200

    Use go modules

--- magefile.go
+++ magefile.go
@@ -7,6 +7,7 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 	"strconv"
@@ -46,6 +47,40 @@ func Build() error {
 	return nil
 }
 
+func Check() error {
+	err := Build()
+	if err != nil {
+		return err
+	}
+
+	err = Vendor()
+	if err != nil {
+		return err
+	}
+
+	// Check if no changes are shown
+	err = sh.RunV("git", "update-index", "--refresh")
+	if err != nil {
+		return err
+	}
+	return sh.RunV("git", "diff-index", "--exit-code", "HEAD", "--")
+}
+
 func Clean() error {
 	return os.RemoveAll(buildDir)
+}
+
+func Vendor() error {
+	fmt.Println(">> mod - updating vendor directory")
+
+	err := sh.RunV("go", "mod", "vendor")
+	if err != nil {
+		return err
+	}
+
+	err = sh.RunV("go", "mod", "verify")
+	if err != nil {
+		return err
+	}
+	return nil
 }
