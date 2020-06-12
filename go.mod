55f9f9b128240f6358b1f62c15358bca441e8141
mtojek <marcin.tojek@elastic.co>
2020-06-12 15:32:38 +0200

    Validate packages

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/elastic/package-storage
 
 go 1.12
 
-require github.com/magefile/mage v1.9.0
+require (
+	github.com/elastic/package-registry v0.4.1-0.20200611125155-d4a6d00d7eed
+	github.com/magefile/mage v1.9.0
+	github.com/pkg/errors v0.9.1
+	gopkg.in/yaml.v2 v2.2.8
+)
--- magefile.go
+++ magefile.go
@@ -8,21 +8,197 @@ package main
 
 import (
 	"fmt"
+	"github.com/pkg/errors"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
+	"path/filepath"
 
 	"github.com/magefile/mage/sh"
+
+	"github.com/elastic/package-registry/util"
 )
 
 var (
-	buildDir     = "./build"
 	packagePaths = []string{"./packages"}
 )
 
-func Build() error {
+type fieldEntry struct {
+	name  string
+	aType string
+}
+
+func Validate() error {
+	paths, err := findPackages()
+	if err != nil {
+		return err
+	}
+
+	for _, path := range paths {
+		err = validatePackage(path)
+		if err != nil {
+			return err
+		}
+	}
 	return nil
 }
 
+func findPackages() ([]string, error) {
+	var matches []string
+	for _, sourceDir := range packagePaths {
+		err := filepath.Walk(sourceDir, func(path string, info os.FileInfo, err error) error {
+			if err != nil {
+				return err
+			}
+
+			f, err := os.Stat(path)
+			if err != nil {
+				return err
+			}
+
+			if !f.IsDir() {
+				return nil // skip as the path is not a directory
+			}
+
+			manifestPath := filepath.Join(path, "manifest.yml")
+			_, err = os.Stat(manifestPath)
+			if os.IsNotExist(err) {
+				return nil
+			}
+			matches = append(matches, path)
+			return filepath.SkipDir
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+	return matches, nil
+}
+
+func validatePackage(path string) error {
+	p, err := util.NewPackage(path)
+	if err != nil {
+		return err
+	}
+
+	err = p.Validate()
+	if err != nil {
+		return errors.Wrapf(err, "package validation failed (path: %s", p.GetPath())
+	}
+
+	datasets, err := p.GetDatasetPaths()
+	if err != nil {
+		return err
+	}
+
+	// Validate if basic stream fields and @timestamp are present
+	for _, dataset := range datasets {
+		datasetPath := filepath.Join(p.BasePath, "dataset", dataset)
+		err = validateRequiredFields(datasetPath)
+		if err != nil {
+			return errors.Wrapf(err, "validating required fields failed (datasetPath: %s)", datasetPath)
+		}
+	}
+	return nil
+}
+
+// validateRequiredFields method loads fields from all files and checks if required fields are present.
+func validateRequiredFields(datasetPath string) error {
+	fieldsDirPath := filepath.Join(datasetPath, "fields")
+
+	// Collect fields from all files
+	var allFields []MapStr
+	err := filepath.Walk(fieldsDirPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		relativePath, err := filepath.Rel(fieldsDirPath, path)
+		if err != nil {
+			return errors.Wrapf(err, "cannot find relative path (fieldsDirPath: %s, path: %s)", fieldsDirPath, path)
+		}
+
+		if relativePath == "." {
+			return nil
+		}
+
+		body, err := ioutil.ReadFile(path)
+		if err != nil {
+			return errors.Wrapf(err, "reading file failed (path: %s)", path)
+		}
+
+		var m []MapStr
+		err = yaml.Unmarshal(body, &m)
+		if err != nil {
+			return errors.Wrapf(err, "unmarshaling file failed (path: %s)", path)
+		}
+
+		allFields = append(allFields, m...)
+		return nil
+	})
+	if err != nil {
+		return errors.Wrapf(err, "walking through fields files failed")
+	}
+
+	// Flatten all fields
+	for i, fields := range allFields {
+		allFields[i] = fields.Flatten()
+	}
+
+	// Verify required keys
+	err = requireField(allFields, "dataset.type", "constant_keyword", err)
+	err = requireField(allFields, "dataset.name", "constant_keyword", err)
+	err = requireField(allFields, "dataset.namespace", "constant_keyword", err)
+	err = requireField(allFields, "@timestamp", "date", err)
+	return err
+}
+
+func requireField(allFields []MapStr, searchedName, expectedType string, validationErr error) error {
+	if validationErr != nil {
+		return validationErr
+	}
+
+	f, err := findField(allFields, searchedName)
+	if err != nil {
+		return errors.Wrapf(err, "finding field failed (searchedName: %s)", searchedName)
+	}
+
+	if f.aType != expectedType {
+		return fmt.Errorf("wrong field type for '%s' (expected: %s, got: %s)", searchedName, expectedType, f.aType)
+	}
+	return nil
+}
+
+func findField(allFields []MapStr, searchedName string) (*fieldEntry, error) {
+	for _, fields := range allFields {
+		name, err := fields.GetValue("name")
+		if err != nil {
+			return nil, errors.Wrapf(err, "cannot get value (key: name)")
+		}
+
+		if name != searchedName {
+			continue
+		}
+
+		aType, err := fields.GetValue("type")
+		if err != nil {
+			return nil, errors.Wrapf(err, "cannot get value (key: type)")
+		}
+
+		if aType == "" {
+			return nil, fmt.Errorf("field '%s' found, but type is undefined", searchedName)
+		}
+
+		return &fieldEntry{
+			name:  name.(string),
+			aType: aType.(string),
+		}, nil
+	}
+	return nil, fmt.Errorf("field '%s' not found", searchedName)
+}
+
 func Check() error {
-	err := Build()
+	err := Validate()
 	if err != nil {
 		return err
 	}
@@ -38,10 +214,6 @@ func Check() error {
 		return err
 	}
 	return sh.RunV("git", "diff-index", "--exit-code", "HEAD", "--")
-}
-
-func Clean() error {
-	return nil
 }
 
 func Vendor() error {
