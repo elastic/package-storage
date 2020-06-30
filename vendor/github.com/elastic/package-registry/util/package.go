2bbc88a942c1652f2128cdf291c3c85a32b348c9
ruflin <spam@ruflin.com>
2020-06-30 11:45:03 +0200

    update registry to most recent version

--- vendor/github.com/elastic/package-registry/util/package.go
+++ vendor/github.com/elastic/package-registry/util/package.go
@@ -6,7 +6,6 @@ package util
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
 	"path"
 	"path/filepath"
@@ -22,40 +21,71 @@ import (
 const defaultType = "integration"
 
 var CategoryTitles = map[string]string{
-	"logs":     "Logs",
-	"metrics":  "Metrics",
-	"security": "Security",
+	"aws":               "AWS",
+	"azure":             "Azure",
+	"cloud":             "Cloud",
+	"config_management": "Config management",
+	"containers":        "Containers",
+	"crm":               "CRM",
+	"custom":            "Custom",
+	"datastore":         "Datastore",
+	"elastic_stack":     "Elastic Stack",
+	"google_loud":       "Google Cloud",
+	"kubernetes":        "Kubernetes",
+	"languages":         "Languages",
+	"message_queue":     "Message Queue",
+	"monitoring":        "Monitoring",
+	"network":           "Network",
+	"notification":      "Notification",
+	"os_system":         "OS & System",
+	"productivity":      "Productivity",
+	"security":          "Security",
+	"support":           "Support",
+	"ticketing":         "Ticketing",
+	"version_control":   "Version Control",
+	"web":               "Web",
+
+	// Old categories, to be removed
+	"logs":    "Logs",
+	"metrics": "Metrics",
+	//"security": "Security",
 }
 
 type Package struct {
+	BasePackage   `config:",inline" json:",inline" yaml:",inline"`
 	FormatVersion string `config:"format_version" json:"format_version" yaml:"format_version"`
 
-	Name          string  `config:"name" json:"name"`
-	Title         *string `config:"title,omitempty" json:"title,omitempty" yaml:"title,omitempty"`
-	Version       string  `config:"version" json:"version"`
-	Readme        *string `config:"readme,omitempty" json:"readme,omitempty" yaml:"readme,omitempty"`
-	License       string  `config:"license,omitempty" json:"license,omitempty" yaml:"license,omitempty"`
-	versionSemVer *semver.Version
-	Description   string       `config:"description" json:"description"`
-	Type          string       `config:"type" json:"type"`
-	Categories    []string     `config:"categories" json:"categories"`
-	Release       string       `config:"release,omitempty" json:"release,omitempty"`
-	Removable     bool         `config:"removable" json:"removable"`
-	Requirement   Requirement  `config:"requirement" json:"requirement"`
-	Screenshots   []Image      `config:"screenshots,omitempty" json:"screenshots,omitempty" yaml:"screenshots,omitempty"`
-	Icons         []Image      `config:"icons,omitempty" json:"icons,omitempty" yaml:"icons,omitempty"`
-	Assets        []string     `config:"assets,omitempty" json:"assets,omitempty" yaml:"assets,omitempty"`
-	Internal      bool         `config:"internal,omitempty" json:"internal,omitempty" yaml:"internal,omitempty"`
-	DataSets      []*DataSet   `config:"datasets,omitempty" json:"datasets,omitempty" yaml:"datasets,omitempty"`
-	Datasources   []Datasource `config:"datasources,omitempty" json:"datasources,omitempty" yaml:"datasources,omitempty"`
-	Download      string       `json:"download" yaml:"download,omitempty"`
-	Path          string       `json:"path" yaml:"path,omitempty"`
+	Readme          *string `config:"readme,omitempty" json:"readme,omitempty" yaml:"readme,omitempty"`
+	License         string  `config:"license,omitempty" json:"license,omitempty" yaml:"license,omitempty"`
+	versionSemVer   *semver.Version
+	Categories      []string         `config:"categories" json:"categories"`
+	Release         string           `config:"release,omitempty" json:"release,omitempty"`
+	Requirement     Requirement      `config:"requirement" json:"requirement"`
+	Screenshots     []Image          `config:"screenshots,omitempty" json:"screenshots,omitempty" yaml:"screenshots,omitempty"`
+	Assets          []string         `config:"assets,omitempty" json:"assets,omitempty" yaml:"assets,omitempty"`
+	ConfigTemplates []ConfigTemplate `config:"config_templates,omitempty" json:"config_templates,omitempty" yaml:"config_templates,omitempty"`
+	Datasets        []*Dataset       `config:"datasets,omitempty" json:"datasets,omitempty" yaml:"datasets,omitempty"`
+	Owner           *Owner           `config:"owner,omitempty" json:"owner,omitempty" yaml:"owner,omitempty"`
 
 	// Local path to the package dir
 	BasePath string `json:"-" yaml:"-"`
 }
 
-type Datasource struct {
+// BasePackage is used for the output of the package info in the /search endpoint
+type BasePackage struct {
+	Name        string     `config:"name" json:"name"`
+	Title       *string    `config:"title,omitempty" json:"title,omitempty" yaml:"title,omitempty"`
+	Version     string     `config:"version" json:"version"`
+	Description string     `config:"description" json:"description"`
+	Type        string     `config:"type" json:"type"`
+	Download    string     `json:"download" yaml:"download,omitempty"`
+	Downloads   []Download `config:"downloads,omitempty" json:"downloads,omitempty" yaml:"downloads,omitempty"`
+	Path        string     `json:"path" yaml:"path,omitempty"`
+	Icons       []Image    `config:"icons,omitempty" json:"icons,omitempty" yaml:"icons,omitempty"`
+	Internal    bool       `config:"internal,omitempty" json:"internal,omitempty" yaml:"internal,omitempty"`
+}
+
+type ConfigTemplate struct {
 	Name        string  `config:"name" json:"name" validate:"required"`
 	Title       string  `config:"title" json:"title" validate:"required"`
 	Description string  `config:"description" json:"description" validate:"required"`
@@ -77,6 +107,10 @@ type Version struct {
 	Max string `config:"max,omitempty" json:"max,omitempty"`
 }
 
+type Owner struct {
+	Github string `config:"github,omitempty" json:"github,omitempty"`
+}
+
 type Image struct {
 	Src   string `config:"src" json:"src" validate:"required"`
 	Title string `config:"title" json:"title,omitempty"`
@@ -86,6 +120,22 @@ type Image struct {
 
 func (i Image) getPath(p *Package) string {
 	return path.Join("/package", p.Name, p.Version, i.Src)
+}
+
+type Download struct {
+	Path string `config:"path" json:"path" validate:"required"`
+	Type string `config:"type" json:"type" validate:"required"`
+}
+
+func NewDownload(p Package, t string) Download {
+	return Download{
+		Path: getDownloadPath(p, t),
+		Type: t,
+	}
+}
+
+func getDownloadPath(p Package, t string) string {
+	return path.Join("/epr", p.Name, p.Name+"-"+p.Version+".tar.gz")
 }
 
 // NewPackage creates a new package instances based on the given base path.
@@ -98,8 +148,7 @@ func NewPackage(basePath string) (*Package, error) {
 	}
 
 	var p = &Package{
-		BasePath:  basePath,
-		Removable: true,
+		BasePath: basePath,
 	}
 	err = manifest.Unpack(p)
 	if err != nil {
@@ -108,9 +157,9 @@ func NewPackage(basePath string) (*Package, error) {
 
 	// Default for the multiple flags is true.
 	trueValue := true
-	for i, _ := range p.Datasources {
-		if p.Datasources[i].Multiple == nil {
-			p.Datasources[i].Multiple = &trueValue
+	for i, _ := range p.ConfigTemplates {
+		if p.ConfigTemplates[i].Multiple == nil {
+			p.ConfigTemplates[i].Multiple = &trueValue
 		}
 	}
 	if p.Type == "" {
@@ -119,7 +168,7 @@ func NewPackage(basePath string) (*Package, error) {
 
 	// If not license is set, basic is assumed
 	if p.License == "" {
-		p.License = "basic"
+		p.License = DefaultLicense
 	}
 
 	if p.Icons != nil {
@@ -134,6 +183,8 @@ func NewPackage(basePath string) (*Package, error) {
 		}
 	}
 
+	p.Downloads = []Download{NewDownload(*p, "tar")}
+
 	if p.Requirement.Kibana.Versions != "" {
 		p.Requirement.Kibana.semVerRange, err = semver.NewConstraint(p.Requirement.Kibana.Versions)
 		if err != nil {
@@ -147,11 +198,6 @@ func NewPackage(basePath string) (*Package, error) {
 
 	if !IsValidRelease(p.Release) {
 		return nil, fmt.Errorf("invalid release: %s", p.Release)
-	}
-
-	p.versionSemVer, err = semver.StrictNewVersion(p.Version)
-	if err != nil {
-		return nil, err
 	}
 
 	readmePath := filepath.Join(p.BasePath, "docs", "README.md")
@@ -177,21 +223,21 @@ func NewPackage(basePath string) (*Package, error) {
 }
 
 func NewPackageWithResources(path string) (*Package, error) {
-	aPackage, err := NewPackage(path)
+	p, err := NewPackage(path)
 	if err != nil {
 		return nil, errors.Wrapf(err, "building package from path '%s' failed", path)
 	}
 
-	err = aPackage.LoadAssets(aPackage.GetPath())
+	err = p.LoadAssets()
 	if err != nil {
 		return nil, errors.Wrapf(err, "loading package assets failed (path '%s')", path)
 	}
 
-	err = aPackage.LoadDataSets()
+	err = p.LoadDataSets()
 	if err != nil {
 		return nil, errors.Wrapf(err, "loading package datasets failed (path '%s')", path)
 	}
-	return aPackage, nil
+	return p, nil
 }
 
 func (p *Package) HasCategory(category string) bool {
@@ -219,13 +265,13 @@ func (p *Package) HasKibanaVersion(version *semver.Version) bool {
 	return true
 }
 
-func (p *Package) IsNewer(pp Package) bool {
-	return p.versionSemVer.GreaterThan(pp.versionSemVer)
+func (p *Package) IsNewerOrEqual(pp Package) bool {
+	return !p.versionSemVer.LessThan(pp.versionSemVer)
 }
 
 // LoadAssets (re)loads all the assets of the package
 // Based on the time when this is called, it might be that not all assets for a package exist yet, so it is reset every time.
-func (p *Package) LoadAssets(packagePath string) (err error) {
+func (p *Package) LoadAssets() (err error) {
 	// Reset Assets
 	p.Assets = nil
 
@@ -255,7 +301,7 @@ func (p *Package) LoadAssets(packagePath string) (err error) {
 		// Strip away the basePath from the local system
 		a = a[len(p.BasePath)+1:]
 
-		a = path.Join("/package", packagePath, a)
+		a = path.Join("/package", p.GetPath(), a)
 		p.Assets = append(p.Assets, a)
 	}
 	return nil
@@ -276,6 +322,8 @@ func collectAssets(pattern string) ([]string, error) {
 	return nil, nil
 }
 
+// Validate is called during Unpack of the manifest.
+// The validation here is only related to the fields directly specified in the manifest itself.
 func (p *Package) Validate() error {
 	if p.FormatVersion == "" {
 		return fmt.Errorf("no format_version set: %v", p)
@@ -312,6 +360,11 @@ func (p *Package) Validate() error {
 		}
 	}
 
+	p.versionSemVer, err = semver.StrictNewVersion(p.Version)
+	if err != nil {
+		return errors.Wrap(err, "invalid package version")
+	}
+
 	err = p.validateVersionConsistency()
 	if err != nil {
 		return errors.Wrap(err, "version in manifest file is not consistent with path")
@@ -329,15 +382,13 @@ func (p *Package) validateVersionConsistency() error {
 	baseDir := filepath.Base(p.BasePath)
 	versionDir, err := semver.NewVersion(baseDir)
 	if err != nil {
+		// TODO: There should be a flag passed to the registry to accept these kind of packages
+		// as otherwise these could hide some errors in the structure of the package-storage
 		return nil // package content is not rooted in version directory
 	}
 
 	if !versionPackage.Equal(versionDir) {
-		manifestVer := "unknown"
-		if p.versionSemVer != nil {
-			manifestVer = p.versionSemVer.String()
-		}
-		return fmt.Errorf("inconsistent versions (path: %s, manifest: %s)", versionDir.String(), manifestVer)
+		return fmt.Errorf("inconsistent versions (path: %s, manifest: %s)", versionDir.String(), p.versionSemVer.String())
 	}
 	return nil
 }
@@ -387,32 +438,9 @@ func (p *Package) LoadDataSets() error {
 			return err
 		}
 
-		// Iterate through all datasources and inputs to find the matching streams and add them to the output.
-		for dK, datasource := range p.Datasources {
-			for iK, _ := range datasource.Inputs {
-				for _, stream := range d.Streams {
-					if stream.Input == p.Datasources[dK].Inputs[iK].Type {
-						if stream.TemplatePath == "" {
-							stream.TemplatePath = "stream.yml.hbs"
-						}
-						stream.Dataset = d.ID
-						streamTemplate := filepath.Join(datasetBasePath, "agent", "stream", stream.TemplatePath)
+		// TODO: Validate that each input specified in a stream also is defined in the package
 
-						streamTemplateData, err := ioutil.ReadFile(streamTemplate)
-						if err != nil {
-							return err
-						}
-
-						stream.TemplateContent = string(streamTemplateData)
-
-						// Add template to stream
-						p.Datasources[dK].Inputs[iK].Streams = append(p.Datasources[dK].Inputs[iK].Streams, stream)
-					}
-				}
-			}
-		}
-
-		p.DataSets = append(p.DataSets, d)
+		p.Datasets = append(p.Datasets, d)
 	}
 
 	return nil
