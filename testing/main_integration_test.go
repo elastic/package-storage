f27dafd70e3e60e215cb478e3e1f1c0cc60461c2
Shaunak Kashyap <ycombinator@gmail.com>
2020-07-14 15:44:54 -0700

    Query experimental packages from registry

--- testing/main_integration_test.go
+++ testing/main_integration_test.go
@@ -128,7 +128,7 @@ type Package struct {
 }
 
 func getPackages(t *testing.T) ([]string, error) {
-	resp, err := http.Get("http://localhost:8080/search")
+	resp, err := http.Get("http://localhost:8080/search?experimental=true")
 	if err != nil {
 		return nil, err
 	}
