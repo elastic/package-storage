de3b7a2eb0d0dc23eae5362a19a44fe31695b671
Jaime Soriano Pastor <jaime.soriano@elastic.co>
2021-10-15 11:17:43 +0200

    Fix import order

--- magefile.go
+++ magefile.go
@@ -10,9 +10,10 @@ import (
 	"os"
 	"path/filepath"
 
-	"github.com/elastic/package-registry/packages"
 	"github.com/magefile/mage/sh"
 	"github.com/pkg/errors"
+
+	"github.com/elastic/package-registry/packages"
 )
 
 var (
